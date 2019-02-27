c9a287261e3e8c374120445791be0be2eaafdbf8
Stef Piatek <stefan@piatek.co.uk>
2019-02-27 17:02:14 +0000

    Add customisable output table fields

--- app/variants/forms.py
+++ app/variants/forms.py
@@ -1,9 +1,14 @@
 from flask_wtf import FlaskForm
 
 from wtforms.validators import DataRequired
-from wtforms import (StringField, IntegerField)
+from wtforms import StringField, SelectField, SelectMultipleField
 
 
 class SearchForm(FlaskForm):
     search = StringField('Search', validators=[DataRequired()])
-
+    FIELD_CHOICES = (
+        ("maf", "MAF"), ("variant_type", "Variant Type"),
+        ("hgvs", "HGVS"), ("consequence", "Consequence"),
+        ("genome_build", "Genome Build"), ("strand", "Strand")
+    )
+    extra_fields = SelectMultipleField("Extra fields", choices=FIELD_CHOICES)
--- app/variants/views.py
+++ app/variants/views.py
@@ -14,12 +14,16 @@ def search():
             search = search.lstrip("chr")
             chrom, positions = search.split(":")
             start, end = [int(x) for x in positions.split('-')]
-            var_list = mongo.db.variants.find(
-                {'$and':[{'mappings.seq_region_name':chrom},
+            var_list = mongo.db.variants.find({
+                '$and':[
+                    {'mappings.seq_region_name':chrom},
                     {'mappings.start':{'$gte':start}},
-                    {'mappings.end':{'$lte':end}}]})
-            return render_template("search.html", form=form, var_list=var_list)
-        else:
+                    {'mappings.end':{'$lte':end}}
+                ]
+            })
+            extra_fields = form.extra_fields.data
+            return render_template("search.html", form=form, var_list=var_list, extra_fields=extra_fields)
+        else:  # form not valid
             return render_template("search.html", form=form)
-    else:
+    else:  # not POST
         return render_template("search.html", form=form)
