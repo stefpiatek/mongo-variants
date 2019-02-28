6369ddacd0af7d71cc9d055bc71e61956238364b
Stef Piatek <stefan@piatek.co.uk>
2019-02-28 14:11:03 +0000

    Add search button

--- app/variants/forms.py
+++ app/variants/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 
 from wtforms.validators import DataRequired
-from wtforms import StringField, SelectMultipleField
+from wtforms import StringField, SelectMultipleField, SubmitField
 
 
 class SearchForm(FlaskForm):
-    search = StringField('Search', validators=[DataRequired()])
+    search = StringField('Search region', validators=[DataRequired()])
     FIELD_CHOICES = (
         ("maf", "MAF"), ("variant_type", "Variant Type"),
         ("hgvs", "HGVS"), ("consequence", "Consequence"),
         ("genome_build", "Genome Build"), ("strand", "Strand")
     )
     extra_fields = SelectMultipleField("Extra fields", choices=FIELD_CHOICES)
+    submit = SubmitField("Search for variants")
