8746ff29869d50952f5637413988f8e5eb46070b
Stef Piatek <stefan@piatek.co.uk>
2019-02-28 14:23:19 +0000

    Validate input

--- app/variants/forms.py
+++ app/variants/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 
 from wtforms.validators import DataRequired
-from wtforms import StringField, SelectMultipleField, SubmitField
+from wtforms import StringField, SelectMultipleField, SubmitField, ValidationError
 
 
 class SearchForm(FlaskForm):
@@ -13,3 +13,15 @@ class SearchForm(FlaskForm):
     )
     extra_fields = SelectMultipleField("Extra fields", choices=FIELD_CHOICES)
     submit = SubmitField("Search for variants")
+
+    def validate_search(self, field):
+        search = field.data.lstrip("chr")
+        base_error = "Please use format chr:start-end"
+        try:
+            chrom, positions = search.split(":")
+        except ValueError:
+            raise ValidationError(f'{base_error}, issue with ":" found')
+        try:
+            start, end = [int(x) for x in positions.split('-')]
+        except ValueError:
+            raise ValidationError(f'{base_error}, issue with "-" found')
